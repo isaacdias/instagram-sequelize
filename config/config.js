ed835be77f56e20f2dec756bd47c4b6693d30c77
Isaac Dias <isaacardias@gmail.com>
2021-04-13 15:10:00 -0300

    alterando o codigo

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "username": "root",
-  "password": "135246",
-  "database": "instagram",
-  "host": "127.0.0.1",
-  "dialect": "mysql"
-}
+    "username": "root",
+    "password": "root",
+    "database": "instagram",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
