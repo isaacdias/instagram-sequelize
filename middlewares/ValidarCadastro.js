3464dd475d63255bf306cd29d78c5366bc5891c6
Isaac Dias <isaacardias@gmail.com>
2021-04-15 09:31:43 -0300

    refatoracao

--- middlewares/ValidarCadastro.js
+++ middlewares/ValidarCadastro.js
@@ -3,10 +3,14 @@ const {Usuario, sequelize} = require('../models')
 module.exports = async (req, res, next) => {
     let {email, nome, senha} = req.body;
     let users = await Usuario.findAll({where: {email} });
-    if(users.length) {
-        res.status(400).json({error: "Email já cadastrado"})
+
+    if(!email){
+        res.status(400).json({error: "Email precisa ser informado"})
+        return;
     } else if (!nome) {
         res.status(400).json({error: "Nome precisa ser informado"})
+    } else if(users.length) {
+        res.status(400).json({error: "Email já cadastrado"})
     } else if (senha.length < 6 || senha.length > 12){
         res.status(400).json({error: "Senha precisa ter mais de 6 e menos que 12 caracteres"})
     }else{
